feffb4c504f2fb6195ab231950bc253bf6339488
pcancio <pcancio@gmail.com>
2022-02-06 16:19:34 -0500

    created post.findall in home routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,33 @@
+const routher = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+            attributes: ['id', 'title', 'description', 'created_at'],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
