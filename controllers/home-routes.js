35d7543c97c755f791b4910b130771f63004fe00
pcancio <pcancio@gmail.com>
2022-02-06 16:29:40 -0500

    finished home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const routher = require('express').Router();
+const router = require('.');
 const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 
@@ -31,3 +32,56 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id', 'description', 'title', 'created_at'],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'no post with this id' });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/login', (req, res) => {
+    console.log(req.session.loggedIn);
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    console.log(req.session.loggedIn);
+    res.render('signup');
+})
+
+module.exports = router;
