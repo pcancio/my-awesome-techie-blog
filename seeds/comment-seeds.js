1672919dbb39ca5009d4e639cafa23e45db5b8da
pcancio <pcancio@gmail.com>
2022-02-08 17:16:29 -0500

    wrote comment seeds

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,26 @@
+const { Comment } = require('../models');
+const commentData = [{
+        comment_text: 'This is some text in this comment.',
+        user_id: 3,
+        post_id: 1
+    }, {
+        comment_text: 'This is some text in this commenting.',
+        user_id: 2,
+        post_id: 1
+    },
+    {
+        comment_text: 'This is some texting in this comment.',
+        user_id: 3,
+        post_id: 2
+    },
+    {
+        comment_text: 'This is something text in this comment.',
+        user_id: 2,
+        post_id: 3
+    },
+    {
+        comment_text: 'This is some text in this comment also.',
+        user_id: 1,
+        post_id: 3
+    }
+];
